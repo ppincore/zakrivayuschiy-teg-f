2f69eae284921c0fb0ce33e152705faea6ce7ac9
Bogdan Gaevskij <liltracyn@yandex.ru>
2024-09-11 17:07:13 +0300

    fix: refactored

--- scripts/set-theme.js
+++ scripts/set-theme.js
@@ -1,6 +1,3 @@
-/* Этот скрипт использует имена классов theme-menu__button, theme-dark, theme-light и theme-auto;
-еще атрибуты disabled и data-theme. Поэтому их нельзя менять в HTML. */
-
 function changeTheme(theme) {
   document.documentElement.className = '';
   document.documentElement.classList.add(`theme-${theme}`);
